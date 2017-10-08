93142245781407857b7c72ba675ed81017e2bc0a
James <jamesmeredith1984@hotmail.com>
2017-10-08 21:51:46 +0100

    crag and home rendering

--- climbing/crag.py
+++ climbing/crag.py
@@ -1,22 +1,21 @@
 import json
 
-def ReadJSON(filename):
-    with open(filename) as data_file:
-        data = json.load(data_file)
-    return data
+
 
 
 class Crag():
-    """ class to hold all of the data about a crag
+    """class to hold all of the data about a crag
     """
-    def __init__(self,**kwargs):
+    def __init__(self,name,description,access,mapsrc,maphref):
+        self.name=name
         self.description=description
         self.access=access
         self.mapsrc=mapsrc
         self.maphref=maphref
 
 
+
 class Climbs():
     """Object to hold the climbs for a Crag
     """
-    
+    pass
--- render.py
+++ render.py
@@ -1,16 +1,47 @@
 from jinja2 import Environment, FileSystemLoader
+import json
+from climbing.crag import Crag
 
 
-def RenderWebsite(data):
+def renderWebsite(crag):
+    """ Renders template using current data
+    """
+    ENV = Environment(loader=FileSystemLoader('./templates'))
+    file_name = 'crag.html'
+    template = ENV.get_template(file_name)
+    html = template.render( crag=crag)
+
+        # Write output in the corresponding HTML file
+
+    with open('./html/grinshill.html', 'w',encoding='utf-8') as out_file:
+        out_file.write(html)
+    return
+
+def renderHome():
     """ Renders template using current data
     """
     ENV = Environment(loader=FileSystemLoader('./templates'))
     file_name = 'index.html'
     template = ENV.get_template(file_name)
-    html = template.render( kioskData=data)
+    html = template.render()
 
         # Write output in the corresponding HTML file
 
     with open('./html/index.html', 'w',encoding='utf-8') as out_file:
         out_file.write(html)
     return
+
+def readJSON(filename):
+    with open(filename) as data_file:
+        data = json.load(data_file)
+    return data
+
+filename='crags/grinshill/crag.json'
+
+cragdata=readJSON(filename)
+
+crag = Crag(name=cragdata["name"],description=cragdata["description"],access=cragdata["access"],\
+    mapsrc=cragdata["mapsrc"],maphref=cragdata["maphref"])
+
+renderWebsite(crag)
+renderHome()
